6c086c69c0e8f16dbea15c90045b4d37f5286a48
Dat T Ng <dattannguyen.saas@gmail.com>
2022-05-05 17:36:29 +0700

    singly-linked-list: add append() & test

--- src/linked-list/singly/singly-linked-list.test.ts
+++ src/linked-list/singly/singly-linked-list.test.ts
@@ -15,6 +15,42 @@ describe('Test SinglyLinkedList prepend()', () => {
 
     expect(ll.head?.value).toBe('newHead')
     expect(ll.head?.next?.value).toBe('head')
+    expect(ll.tail?.value).toBe('head')
+  })
+
+})
+
+describe('Test SinglyLinkedList append()', () => {
+
+  it('Should_AppendNewNode_WhenHasNoHead', () => {
+    const ll = new SinglyLinkedList()
+    ll.append('head')
+
+    expect(ll.head?.value).toBe('head')
+    expect(ll.tail).toBe(undefined)
+  })
+
+  it('Should_AppendNewNode_WhenHasHead&NoTail', () => {
+    const ll = new SinglyLinkedList()
+    ll.append('head')
+    ll.append('tail')
+
+    expect(ll.head?.value).toBe('head')
+    expect(ll.tail?.value).toBe('tail')
+    expect(ll.head?.next).toBe(ll.tail)
+  })
+
+  it('Should_AppendNewNode_WhenHasHead&Tail', () => {
+    const ll = new SinglyLinkedList()
+    ll.append(1).append(2)
+
+    ll.append(3)
+    expect(ll.head?.next?.value).toBe(2)
+    expect(ll.tail?.value).toBe(3)
+
+    ll.append('tail2')
+    expect(ll.head?.next?.next?.next.value).toBe('tail2')
+    expect(ll.tail?.value).toBe('tail2')
   })
 
 })
--- src/linked-list/singly/singly-linked-list.ts
+++ src/linked-list/singly/singly-linked-list.ts
@@ -2,14 +2,43 @@ import { SinglyLinkedListNode } from './singly-linked-list-node'
 
 
 export class SinglyLinkedList {
+
   private _head: SinglyLinkedListNode
+  private _tail: SinglyLinkedListNode
+
 
   get head(): SinglyLinkedListNode | undefined {
     return this._head
   }
 
+  get tail(): SinglyLinkedListNode | undefined {
+    return this._tail
+  }
+
   prepend(value: unknown): SinglyLinkedList {
-    this._head = new SinglyLinkedListNode(value, this._head)
+    if (this._head) {
+      this._tail = new SinglyLinkedListNode(this._head.value)
+      return this
+    }
+
+    this._head = new SinglyLinkedListNode(value, this._tail)
+    return this
+  }
+
+  append(value: unknown): SinglyLinkedList {
+    if (!this._head) {
+      return this.prepend(value)
+    }
+
+    const newNode = new SinglyLinkedListNode(value)
+    if (!this._tail) {
+      this._tail = newNode
+      this._head.next = newNode
+    } else {
+      this._tail.next = newNode
+      this._tail = newNode
+    }
+
     return this
   }
 }
