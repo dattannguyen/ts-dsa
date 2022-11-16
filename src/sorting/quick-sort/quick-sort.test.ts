7000bdced3d529a169b1619975635d22f2477200
Dat T Ng <dattannguyen.saas@gmail.com>
2022-11-16 18:12:57 +0700

    sorting: add more test

--- src/sorting/quick-sort/quick-sort.test.ts
+++ src/sorting/quick-sort/quick-sort.test.ts
@@ -13,5 +13,8 @@ describe('Test quickSort()', () => {
     const second = quickSort([...secondNumbers], onTraverse)
     expect(second.join(',')).toBe(secondNumbers.reverse().join(','))
     expect(onTraverse.mock.calls.length).toBeLessThanOrEqual(second.length * second.length)
+
+    const third = quickSort([15, 8, 5, 12, 10, 1, 16, 9, 11, 7, 20, 3, 2, 6, 17, 18, 4, 13, 14, 19])
+    expect(third.join(',')).toBe([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].join(','))
   })
 })
